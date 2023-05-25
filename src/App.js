31f80bfadb73fd0a9b64248e54c47f136fb3c6ee
unknown <Shtya54@gmail.com>
2023-05-26 01:14:34 +0300

    First

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState , useRef , useEffect} from "react"
+import {motion} from "framer-motion"
+import img1 from "./images/1.jpeg"
+import img2 from "./images/2.jpeg"
+import img3 from "./images/3.jpeg"
+import img4 from "./images/4.jpeg"
+import img5 from "./images/5.jpeg"
+import img6 from "./images/6.jpeg"
+import img7 from "./images/7.jpeg"
+
 
 function App() {
+  const [width , setwidth] = useState(0)
+  const carouselRef = useRef()
+  useEffect(_=> setwidth(carouselRef.current.scrollWidth - carouselRef.current.offsetWidth) ,[])
+
+  const imgs = [img1 , img2 , img3 , img4 , img5 ,img6 , img7]
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <motion.div className="carousel" ref={carouselRef} >
+        <motion.div drag="x" dragConstraints={{right:0 ,left:-width }}  className="inner-carousel">
+          {
+            imgs?.map((e,index)=> (
+              <motion.div key={index} className="item" whileTap={{ cursor :"grabbing"}}>
+                <img src={e} className="item-img" />
+              </motion.div>
+            ))
+          }
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
